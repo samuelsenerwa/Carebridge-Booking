d9acccf91a1674e626cc02fb056694cf2be8c4c6
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-07 21:48:40 +0300

    initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        PrimaryColor: "#0067FF",
+        yellowColor: "#FEB60D",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#01B5C5",
+        headingColor: "#181A11E",
+        textColor: "#4E545F",
+      },
+
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 40px 100px 0px;",
+      },
+    },
+  },
+  plugins: [],
+};
