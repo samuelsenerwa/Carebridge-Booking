9d3c8a681048f601001ed68397f5a1977456a352
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-08 09:46:06 +0300

    initial-commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-     <h1 className='text-[20px] text-red-600'>Hello react + Vite</h1>
+     <h1 className='text-[20px] text-blue-600'>Hello react + Vite</h1>
     </>
   )
 }
