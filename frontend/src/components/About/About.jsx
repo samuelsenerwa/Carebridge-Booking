280cce73cb357d41c67a058a1a19e0eb91530493
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-09 22:47:15 +0300

    about  content

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import aboutImg from "../../assets/images/about.png";
 import aboutCardImg from "../../assets/images/about-card.png";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -13,6 +14,26 @@ const About = () => {
             <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right[-7%] lg:right-[22%]">
               <img src={aboutCardImg} alt="" />
             </div>
+          </div>
+
+          {/* =============about content ============ */}
+          <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2">
+            <h2 className="heading">Proud to be one of the nations best</h2>
+            <p className="text__para">
+              For 30 years in a row, U.S News &/ World Report has recognized us
+              as one of the best publics hospitals in the Nation and #1 in
+              Texas. Lorem ipsum dolor sit amet consecteur, adispiscing elit.
+              Quas, memo?
+            </p>
+            <p className="text__para mt-[30px]">
+              Our best is something we strive for each day, caring for our
+              patients-not looking back at what we accomplished but towards what
+              we can do tomorrow. Providing the best.
+            </p>
+
+            <Link to="/">
+              <button className="btn">Learn More</button>
+            </Link>
           </div>
         </div>
       </div>
