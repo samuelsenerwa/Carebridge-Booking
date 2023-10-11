47ca4a05b0bf942e5fb6e98c210e9e038668ebc3
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-11 15:17:33 +0300

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,172 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineFacebook,
+  AiFillFacebook,
+} from "react-icons/ai";
+
+const socialLinks = [
+  {
+    path: "", //enter your link here
+    icon: <AiFillYoutube className="grop-hover:text-white w-4 h-5" />,
+  },
+
+  {
+    path: "", //enter your link here
+    icon: <AiFillGithub className="grop-hover:text-white w-4 h-5" />,
+  },
+
+  {
+    path: "", //enter your link here
+    icon: <AiFillFacebook className="grop-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "", //enter your link here
+    icon: <RiLinkedinFill className="grop-hover:text-white w-4 h-5" />,
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/",
+    display: "About Us",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/",
+    display: "Blog",
+  },
+];
+
+const quickLinks02 = [
+  {
+    path: "/find-a-doctor",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/",
+    display: "Request an appointment",
+  },
+  {
+    path: "/",
+    display: "Get opinion",
+  },
+];
+
+quickLinks03 = [
+  {
+    path: "/",
+    display: "Donate",
+  },
+  {
+    path: "/contact",
+    display: "Contact Us",
+  },
+];
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <footer className="pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div>
+            <img src={logo} alt="" />
+            <p className="text-[16px] leading-7 font-[400] text-textColor">
+              Copyright {year} developed by Samuel Senerwa all rights reserved.
+            </p>
+
+            <div className="flex items-center gap-3 mt-4">
+              {socialLinks.map((link, index) => (
+                <Link
+                  to={link.path}
+                  key={index}
+                  className="w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none:"
+                >
+                  {link.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Quick Links
+            </h2>
+            <ul>
+              {quickLinks01.map(
+                (item,
+                (index) => (
+                  <li key={index} className="mb-4">
+                    <Link
+                      to={item.path}
+                      className="text-[16px] leading-7 font-400 text-textColor"
+                    >
+                      {item.display}
+                    </Link>
+                  </li>
+                ))
+              )}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I want to:
+            </h2>
+            <ul>
+              {quickLinks02.map(
+                (item,
+                (index) => (
+                  <li key={index} className="mb-4">
+                    <Link
+                      to={item.path}
+                      className="text-[16px] leading-7 font-400 text-textColor"
+                    >
+                      {item.display}
+                    </Link>
+                  </li>
+                ))
+              )}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Support
+            </h2>
+            <ul>
+              {quickLinks03.map(
+                (item,
+                (index) => (
+                  <li key={index} className="mb-4">
+                    <Link
+                      to={item.path}
+                      className="text-[16px] leading-7 font-400 text-textColor"
+                    >
+                      {item.display}
+                    </Link>
+                  </li>
+                ))
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
