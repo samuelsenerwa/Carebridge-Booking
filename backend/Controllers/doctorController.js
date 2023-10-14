83866644ca2f34bc7e61ab51a043bc827f0affcf
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-14 23:37:42 +0300

    added PUT, GET methods for API

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -56,8 +56,27 @@ export const getSingleDoctor = async (req, res) => {
 
 export const getAllDoctor = async (req, res) => {
   //   const id = req.params.id;
+
+  // Search paremeter
+
   try {
-    const doctors = await Doctor.find({}).select("-password");
+    const { query } = req.query;
+    let doctors;
+    if (query) {
+      doctors = await Doctor.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { specialization: { $regex: query, $options: "i" } },
+        ],
+      }).select("-password");
+    } else {
+      doctors = await Doctor.find({ isApproved: "approved" }).select(
+        "-password"
+      );
+    }
+
+    // const doctors = await Doctor.find({}).select("-password");
     res.status(200).json({
       success: true,
       message: "Doctors found",
--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -48,8 +48,9 @@ const DoctorCard = ({ doctor }) => {
             At {hospital}
           </p>
         </div>
+        {/* fix the display of doctor by ID */}
         <Link
-          to="/doctors:id"
+          to="/doctors"
           className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none"
         >
           <BsArrowRight className="group-hover:text-white w-6 h-5" />
